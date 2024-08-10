2f540be9f8bf238d75448dcf16d96fe8c28ab276
Gabriel <gabriel.costache@mobilityhouse.com>
2024-08-10 10:41:48 +0200

    fixing dart issues

--- netcode_client/lib/src/netcode_client_base.dart
+++ netcode_client/lib/src/netcode_client_base.dart
@@ -4,3 +4,17 @@
 class Awesome {
   bool get isAwesome => true;
 }
+
+class NetcodeClient {
+  // Authenticates the client with the backend
+  Future<void> authenticate() async {}
+
+  // Request the backend to play a game and receive a connect token
+  Future<void> play() async {}
+
+  // Use the connect token to create a UDP connection with the game server
+  Future<void> connectToServer() async {}
+
+  // Send packages over the UDP connection created
+  Future<void> sendPackage(dynamic data) async {}
+}
--- netcode_core/example/netcode_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:netcode/netcode.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
