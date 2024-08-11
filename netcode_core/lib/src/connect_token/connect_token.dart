131149f1f2641040fe5a1a0169a1ac47b7b83bfd
Gabriel <gabriel.costache@mobilityhouse.com>
2024-08-11 18:34:06 +0200

    toByteData method for connect token

--- netcode_core/lib/src/connect_token/connect_token.dart
+++ netcode_core/lib/src/connect_token/connect_token.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:netcode_core/src/address_endpoint.dart';
@@ -49,5 +50,104 @@ class ConnectToken {
     _decryptedToken.complete(decrypted);
 
     return decrypted;
+  }
+
+  ByteData toByteData() {
+    final data = ByteData(2048);
+    int offset = 0;
+
+    final version = this.version.asAscii;
+
+    for (int i = 0; i < 13; i++) {
+      if (i < version.length) {
+        data.setUint8(offset, version[i]);
+      }
+    }
+    offset += 13;
+
+    data.setUint64(
+      offset,
+      protocolId.toUnsigned(64),
+      Endian.little,
+    );
+    offset += 8;
+
+    data.setUint64(
+      offset,
+      createdAt.toUnsigned(64),
+      Endian.little,
+    );
+    offset += 8;
+
+    data.setUint64(
+      offset,
+      expiresAt.toUnsigned(64),
+      Endian.little,
+    );
+    offset += 8;
+
+    for (int i = 0; i < nonce.length; i++) {
+      if (i < version.length) {
+        data.setUint8(offset, nonce[i]);
+      }
+      offset++;
+    }
+    offset += nonce.lengthInBytes;
+
+    for (int i = 0; i < 1024; i++) {
+      if (i < encryptedPrivateToken.length) {
+        data.setUint8(offset, encryptedPrivateToken[i]);
+      }
+      offset++;
+    }
+
+    data.setUint32(
+      offset,
+      serverAddresses.length.toUnsigned(32),
+      Endian.little,
+    );
+    offset += 4;
+
+    for (final endpoint in serverAddresses) {
+      if (endpoint.address.type == InternetAddressType.IPv4) {
+        data.setUint8(
+          offset,
+          1.toUnsigned(8),
+        );
+      } else if (endpoint.address.type == InternetAddressType.IPv6) {
+        data.setUint8(
+          offset,
+          2.toUnsigned(8),
+        );
+      } else {
+        throw Exception('Address type not valid');
+      }
+
+      offset += 1;
+
+      for (final v in endpoint.address.rawAddress) {
+        data.setUint8(offset, v);
+        offset++;
+      }
+
+      data.setUint16(
+        offset,
+        endpoint.port.toUnsigned(16),
+        Endian.little,
+      );
+      offset += 2;
+    }
+
+    for (final key in clientToServerKey) {
+      data.setUint8(offset, key);
+      offset++;
+    }
+
+    for (final key in serverToClientKey) {
+      data.setUint8(offset, key);
+      offset++;
+    }
+
+    return data;
   }
 }
